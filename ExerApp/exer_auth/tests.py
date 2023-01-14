4cb18047e3468f307ef3dca9a1c0fc0298f76f58
Korek-F <filip.korycki2222@gmail.com>
2023-01-14 17:13:36 +0100

    V1.1.3 Exer auth tests

--- ExerApp/exer_auth/tests.py
+++ ExerApp/exer_auth/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import User
+from django.urls import reverse
+from django.contrib import auth
 
-# Create your tests here.
+class TestExerAuhtModels(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('john','john@elek.pl','johnhardpassowrd1')
+
+    def test_user_model(self):
+        user = User.objects.get(id=1)
+        self.assertEqual(user.username, 'john')
+        self.assertEqual(user.email, 'john@elek.pl')
+
+class TestExerAuthViews(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.user = User.objects.create_user('john','john@elek.pl','johnhardpassowrd1')
+    
+    def test_login_and_logout_view(self):
+        user = auth.get_user(self.c)
+        self.assertEqual(user.is_authenticated, False)
+        response = self.c.get(reverse("login"))
+        self.assertEqual(response.status_code,200)
+        response = self.c.post(reverse('login'),{'username':'john','password':'johnhardpassowrd1'})
+        self.assertEqual(response.status_code,302)
+        user = auth.get_user(self.c)
+        self.assertEqual(user.is_authenticated, True)
+
+        #Logout
+        response = self.c.post(reverse("logout"))
+        self.assertEqual(response.status_code,302)
+        user = auth.get_user(self.c)
+        self.assertEqual(user.is_authenticated, False)
+
+
+
+
+        
