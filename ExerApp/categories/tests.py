b4ecd602ca994e5d8fa0a39de27ba78b6b87084e
unknown <filip.korycki2222@gmail.com>
2023-01-30 17:46:56 +0100

    V1.3.7 Categories tests

--- ExerApp/categories/tests.py
+++ ExerApp/categories/tests.py
@@ -38,3 +38,23 @@ class TestCategoriesViews(TestCase):
         response = self.c.get(reverse('category_detail', kwargs={"name":"test_category_1"}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "test_category_1")
+    
+    def test_add_and_delete_category_view(self):
+        response = self.c.post(reverse('add_cat_to_set', kwargs={"set_id":1}),{"set_id":1,"name":"test_cat_name7645"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.exercise_set.categories.all().count(),1)
+        self.assertEqual(self.exercise_set.categories.first().name,"TEST_CAT_NAME7645")
+
+        response = self.c.post(reverse('delete_cat_from_set', kwargs={"set_id":1}),{"set_id":1,"delete_id":1})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.exercise_set.categories.all().count(),0)
+
+        for i in range(6):
+            self.c.post(reverse('add_cat_to_set', kwargs={"set_id":1}),{"set_id":1,"name":"test_cat_name7645"+str(i)})
+        self.assertEqual(self.exercise_set.categories.all().count(),5)
+
+    
+    
+
+
+
